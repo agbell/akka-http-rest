5b79c69c3cd45178b03e59fe4e75c72c32a66414
Adam Bell <agbell@gmail.com>
2016-10-23 14:11:42 +0000

    SAVEPOINT

--- src/main/scala/me/archdev/restapi/utils/DatabaseService.scala
+++ src/main/scala/me/archdev/restapi/utils/DatabaseService.scala
@@ -10,6 +10,5 @@ class DatabaseService() {
   import driver.api._
   val dbConfig: DatabaseConfig[PostgresDriver] = DatabaseConfig.forConfig("pgdb")
   val db = dbConfig.db
-//  val db = Database.forConfig("pgdb")
   db.createSession()
 }
